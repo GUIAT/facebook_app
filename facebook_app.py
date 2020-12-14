a7795aba76afe8af7c281598f11ab4de14583a16
Gui <guillaume.a.tissier@gmail.com>
2020-12-14 18:12:06 -0300

    App send back the needed verification data

--- facebook_app.py
+++ facebook_app.py
@@ -4,11 +4,37 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-# VARIABLES
+# VARIABLES not sure if keep
+
 token = '12345'
+verification_parameters = []
+
 
 
 class Verification(Resource): # Not Student anymore
+    parser = reqparse.RequestParser()
+    parser.add_argument('hub.mode', type=str)
+    parser.add_argument('hub.challenge', type=str)
+    parser.add_argument('hub.verify_token', type=str)
+
+    
+    def get (self):
+        received_data = Verification.parser.parse_args()
+        verification_parameters.append(received_data)
+
+        if token == received_data['hub.verify_token']:
+            return received_data['hub.challenge']
+
+    def post (self):
+        return verification_parameters
+
+api.add_resource(Verification, '/verification') # Not Student anymore
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+    '''
+    class Verification(Resource): # Not Student anymore
     parser = reqparse.RequestParser()
     parser.add_argument('hub.mode', type=str)
     parser.add_argument('hub.challenge', type=str)
@@ -20,14 +46,7 @@ class Verification(Resource): # Not Student anymore
                             'hub.mode' : received_data['hub.mode'],
                             'hub.challenge' : received_data['hub.challenge'],
                             'hub.verify_token' : received_data['hub.verify_token']
-
                             }
-
-        return verification_data
-
-
-
-api.add_resource(Verification, '/verification') # Not Student anymore
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+        if token == received_data['hub.verify_token']:
+            return {"1" : "Good"}
+    '''
