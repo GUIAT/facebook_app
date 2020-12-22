5a9ccea56d04d762ed3ad8903da54b3bf415a7f5
Gui <guillaume.a.tissier@gmail.com>
2020-12-22 16:44:49 -0300

    with Json request

--- facebook_app.py
+++ facebook_app.py
@@ -1,4 +1,10 @@
-from flask import Flask, jsonify
+# TO DO : VALIDATION - OK
+# TO DO : GET JSON - PENDING
+# facebook documentation to follow : 'check event notification' 
+# https://developers.facebook.com/docs/graph-api/webhooks/getting-started?locale=en_US
+# Sample app :https://developers.facebook.com/docs/graph-api/webhooks/sample-apps
+# video taht might help : https://www.youtube.com/watch?v=82cpdEisqsA&t=246s
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
@@ -6,8 +12,9 @@ api = Api(app)
 
 # VARIABLES not sure if keep
 
-token = '12345'
+token = '12345' #figure out a way to get this from heroku
 verification_parameters = []
+received_updates = []
 
 
 
@@ -25,28 +32,19 @@ class Verification(Resource): # Not Student anymore
         if token == received_data['hub.verify_token']:
             return int(received_data['hub.challenge'])
 
-    def post (self):
-        return verification_parameters
+#    def post (self):
+#        return verification_parameters
 
+# PAYLOAD
+#class Payload(Resource): 
+    def post (self):
+        update = request.get_json()
+        received_updates.append(update)
+        return received_updates, 404
+
+# APP NECESSITIES
 api.add_resource(Verification, '/verification') # Not Student anymore
+#api.add_resource(Payload, '/stories') # Not Student anymore
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
-    '''
-    class Verification(Resource): # Not Student anymore
-    parser = reqparse.RequestParser()
-    parser.add_argument('hub.mode', type=str)
-    parser.add_argument('hub.challenge', type=str)
-    parser.add_argument('hub.verify_token', type=str)
-    
-    def get (self):
-        received_data = Verification.parser.parse_args()
-        verification_data = {
-                            'hub.mode' : received_data['hub.mode'],
-                            'hub.challenge' : received_data['hub.challenge'],
-                            'hub.verify_token' : received_data['hub.verify_token']
-                            }
-        if token == received_data['hub.verify_token']:
-            return {"1" : "Good"}
-    '''
